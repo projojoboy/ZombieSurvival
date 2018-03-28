6602b7e2d129af00842fd870a4549f2eaef0667d
Projojoboy <22048@ma-web.nl>
2018-03-28 14:04:52 +0200

    Updated the loot system with particles(Casper proof)

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float speed;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, v, 0);
+
+        rb.velocity = move * Time.fixedDeltaTime * speed * 100;
+	}
+}
