562859957ebfd918b6e5047e7d2637001ff967c2
Projojoboy <22048@ma-web.nl>
2018-03-29 12:53:57 +0200

    Added Shooting

---
+++ Assets/Scripts/Player/Weapons/MoveTrail.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrail : MonoBehaviour {
+
+    [SerializeField] int speed = 230;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        Destroy(gameObject, 1);
+	}
+}
---
+++ Assets/Scripts/Player/Weapons/Weapon.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    [SerializeField] float fireRate = 0;
+    [SerializeField] float damage = 10;
+    [SerializeField] LayerMask whatToHit;
+
+    [SerializeField] Transform bulletTrailPref;
+
+    float timeToSpawnEffect = 0;
+    float effectSpawnRate = 10;
+
+    float timeToFire = 0;
+    Transform shootPoint;
+	// Use this for initialization
+	void Awake () {
+        shootPoint = transform.FindChild("ShootPoint");
+        if(shootPoint == null)
+        {
+            Debug.LogError("No Shootpoint? WHAT?!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if(Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 shootPointPosition = new Vector2(shootPoint.position.x, shootPoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(shootPointPosition, mousePosition - shootPointPosition, 100, whatToHit);
+
+        if(Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+
+        Debug.DrawLine(shootPointPosition, (mousePosition-shootPointPosition)*100, Color.cyan);
+        if(hit.collider != null)
+        {
+            Debug.DrawLine(shootPointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " and did " + damage + " damage!");
+        }
+    }
+
+    void Effect()
+    {
+        Instantiate(bulletTrailPref, shootPoint.position, shootPoint.rotation);
+    }
+}
